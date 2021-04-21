f0e787b8db18c6c5ce7fd23df9959fbc94e040f1
brianmcilwrath <brian.mcilwrath@stfc.ac.uk>
2021-04-21 18:59:28 +0100

    SIM-831: Add SDPMaster tests

--- tests/features/master.feature
+++ tests/features/master.feature
@@ -1,7 +1,24 @@
 Feature: SDP Master Device
 
-    Scenario: On command succeeds
-        Given the SDP Master device
-        And its state is STANDBY
-        When I call On
-        Then its state should become ON
+    Scenario Outline: Command succeeds in allowed state
+        Given I have an SDPMaster device
+        When the state is <initial_state>
+        And I call <command>
+        Then the state should be <final_state>
+#        And the log should contain a transaction ID
+
+        Examples:
+        | command | initial_state | final_state |
+        | Off     | STANDBY       | OFF         |
+        | Off     | DISABLE       | OFF         |
+        | Off     | ON            | OFF         |
+        | Standby | OFF           | STANDBY     |
+        | Standby | DISABLE       | STANDBY     |
+        | Standby | ON            | STANDBY     |
+        | Disable | OFF           | DISABLE     |
+        | Disable | STANDBY       | DISABLE     |
+        | Disable | ON            | DISABLE     |
+        | On      | OFF           | ON          |
+        | On      | STANDBY       | ON          |
+        | On      | DISABLE       | ON          |
+
--- tests/test_master.py
+++ tests/test_master.py
@@ -5,44 +5,99 @@ from time import sleep
 import pytest
 import tango
 
-
-from pytest_bdd import scenario, given, when, then
+from pytest_bdd import scenarios, given, when, then, parsers
 
 logger = logging.getLogger(__name__)
 
-def wait_for_change(state):
+#@pytest.fixture
+#def sdp_config():
+#   return ska_sdp_config.Config()
+
+
+scenarios("features/master.feature")
+
+# -----------
+# Given steps
+# -----------
+
+@given("I have an SDPMaster device", target_fixture='master_device')
+def connect_to_master_device():
+   device = tango.DeviceProxy("test_sdp/elt/master")
+   return device
+   
+# ----------
+# Then steps
+# ----------
+
+@when(parsers.parse('the state is {initial_state:S}'))
+@when('the state is <initial_state>')
+def set_device_state(master_device, initial_state):
+    """Set the device state.
+
+    :param master_device: SDPMaster device
+    :param state_value: desired device state
+
+    """
+    # Set the device state if incorrect
+    if(master_device.state() != tango.DevState.names[initial_state]):
+        # Get command function
+       command_func = getattr(master_device, initial_state)
+       # Call the command
+       command_func()
+            
+       # Check that state has been set correctly
+ 
+       wait_for_change(master_device, initial_state)    
+
+@when(parsers.parse('I call {command:S}'))
+@when('I call <command>')
+def command(master_device, command):
+    """Call the device commands.
+
+    :param master_device: SDPMaster device
+    :param command: name of command to call
+
+    """
+    # Check command is present
+    command_list = master_device.get_command_list()
+    assert command in command_list
+    
+    # Get command function
+    command_func = getattr(master_device, command)
+    # Call the command
+    command_func()
+
+# ----------
+# Then steps
+# ----------
+
+@then(parsers.parse('the state should be {final_state:S}'))
+@then('the state should be <final_state>')
+def check_device_state(master_device, final_state):
+    """Check the device state.
+
+    :param master_device: SDPMaster device
+    :param final_state: expected state value
+
+    """
+    if(master_device.state() != tango.DevState.names[final_state]):
+       wait_for_change(master_device, final_state)
+    
+# -----------------------------------------------------------------------------
+# Ancillary functions
+# -----------------------------------------------------------------------------
+
+def wait_for_change(device, state):
+   """ Wait for any processing time un til device state changes
+   
+   :param device: Tamgo device DevProxy
+   :param state: The expected state
+   """
    for _ in range(10):
-      if pytest.dp.State() == state:
+      if device.State() == tango.DevState.names[state]:
          break
       sleep(1)
    else:
       pytest.fail("State failed to change after 10 seconds!")
    return
 
-@pytest.fixture
-def global_var():
-   pytest.dp = None
-
-@scenario("features/master.feature", "On command succeeds")
-def test_on_command():
-   pass
-
-@given("the SDP Master device")
-def connect_to_master_device():
-   pytest.dp = tango.DeviceProxy("test_sdp/elt/master")
-
-@given("its state is STANDBY")
-def check_standby_state():
-   if pytest.dp.State() != tango._tango.DevState.STANDBY:
-      pytest.dp.Standby()
-   wait_for_change(tango._tango.DevState.STANDBY)
-
-@when("I call On")
-def call_on_command():
-   pytest.dp.On()
-
-@then("its state should become ON")
-def check_state_change():
-   wait_for_change(tango._tango.DevState.ON)
-   logger.info("State change to ON")
-     
