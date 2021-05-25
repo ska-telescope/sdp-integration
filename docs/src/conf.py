80076fdd1d5e4bee96aee502f439cb437806092a
Mark Ashdown <maja1@mrao.cam.ac.uk>
2021-05-25 18:39:39 +0100

    Release 0.5.0

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -36,9 +36,9 @@ author = "SKA SDP Developers"
 # built documents.
 #
 # The short X.Y.Z version.
-version = "0.4.1"
+version = "0.5.0"
 # The full version, including alpha/beta/rc pre-release tags.
-release = "0.4.1"
+release = "0.5.0"
 
 # -- General configuration ------------------------------------------------
 
